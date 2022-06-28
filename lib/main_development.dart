2587974511a1b4865d64ecb214d84093c3a5154b
Conrad Hoang <tcmhoang@outlook.com>
2022-06-28 04:14:16 -0400

    feat!: integrate revup_core pkg to customer app

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,18 @@
-import 'package:revup/app/app.dart';
-import 'package:revup/bootstrap.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:revup_core/core.dart';
+
+import 'package:revup/router/app_router.dart';
+import 'l10n/l10n.dart';
 
 void main() {
-  bootstrap(() => const App());
+  final appRouter = AppRouter();
+  bootstrap(
+    tuple2(appRouter, appRouter.defaultRouteParser()),
+    const [
+      AppLocalizations.delegate,
+      GlobalMaterialLocalizations.delegate,
+    ],
+    AppLocalizations.supportedLocales,
+  );
 }
