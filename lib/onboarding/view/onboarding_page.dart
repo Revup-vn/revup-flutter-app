7a99e678336ab5b3c226ea27e1e418905c3190f3
cantgim <cantgim@gmail.com>
2022-06-30 11:35:56 +0700

    fix: remove background page & update position controls
    refactor: update localization naming

--- lib/onboarding/view/onboarding_page.dart
+++ lib/onboarding/view/onboarding_page.dart
@@ -11,43 +11,41 @@ class OnboardingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
-    const widthSizedBox = 350.0;
     const pageDecoration = PageDecoration(
       titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
       titlePadding: EdgeInsets.zero,
       bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
-      pageColor: Colors.white,
       imagePadding: EdgeInsets.only(top: 30.0),
     );
     final pageViewModels = [
       PageViewModel(
-        title: l10n.introPageFirstTitle,
+        title: l10n.onboardPageFirstTitle,
         body: '',
         image: Center(
           child: SizedBox(
-            width: widthSizedBox,
+            width: 350.0,
             child: Assets.screens.orderConfirm.svg(),
           ),
         ),
         decoration: pageDecoration,
       ),
       PageViewModel(
-        title: l10n.introPageSecondTitle,
+        title: l10n.onboardPageSecondTitle,
         body: '',
         image: Center(
           child: SizedBox(
-            width: widthSizedBox,
+            width: 350.0,
             child: Assets.screens.destination.svg(),
           ),
         ),
         decoration: pageDecoration,
       ),
       PageViewModel(
-        title: l10n.introPageThirdTitle,
+        title: l10n.onboardPageThirdTitle,
         body: '',
         image: Center(
           child: SizedBox(
-            width: widthSizedBox,
+            width: 350.0,
             child: Assets.screens.mobilePay.svg(),
           ),
         ),
@@ -55,26 +53,26 @@ class OnboardingPage extends StatelessWidget {
       ),
     ];
 
-    return IntroductionScreen(
-      pages: pageViewModels,
-      showSkipButton: true,
-      onDone: () => context.router.push(const TestRoute()),
-      onSkip: () => context.router.push(const TestRoute()),
-      skip: Text(l10n.txtBtnSkip),
-      done: Text(l10n.txtBtnDone),
-      next: Text(l10n.txtBtnNext),
-      skipStyle: TextButton.styleFrom(primary: Colors.black),
-      doneStyle: TextButton.styleFrom(primary: Colors.black),
-      nextStyle: TextButton.styleFrom(primary: Colors.black),
-      controlsPosition: const Position(left: 0, right: 0, bottom: 100),
-      controlsMargin: const EdgeInsets.all(16),
-      controlsPadding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
-      dotsDecorator: const DotsDecorator(
-        activeColor: Color(0xFFFCD04E),
-        size: Size(10.0, 10.0),
-        activeSize: Size(25.0, 10.0),
-        activeShape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+    return SafeArea(
+      child: IntroductionScreen(
+        pages: pageViewModels,
+        showSkipButton: true,
+        onDone: () => context.router.push(const TestRoute()),
+        onSkip: () => context.router.push(const TestRoute()),
+        skip: Text(l10n.btnSkipText),
+        done: Text(l10n.btnNextText),
+        next: Text(l10n.btnDoneText),
+        skipStyle: TextButton.styleFrom(primary: Colors.black),
+        doneStyle: TextButton.styleFrom(primary: Colors.black),
+        nextStyle: TextButton.styleFrom(primary: Colors.black),
+        controlsPadding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+        dotsDecorator: const DotsDecorator(
+          activeColor: Color(0xFFFCD04E),
+          size: Size(10.0, 10.0),
+          activeSize: Size(25.0, 10.0),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25.0)),
+          ),
         ),
       ),
     );
