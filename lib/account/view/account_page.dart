9f362fda8bc9a0bb60f6ff2bbf329fc019f8f025
Nam Ngoc <namngoc231@gmail.com>
2022-07-01 16:24:05 +0700

    fix: implement account page and signup page

--- lib/account/view/account_page.dart
+++ lib/account/view/account_page.dart
@@ -4,34 +4,33 @@ import 'package:revup/l10n/l10n.dart';
 void main() => runApp(const AccountPage());
 
 class AccountPage extends StatelessWidget {
-  const AccountPage({Key? key}) : super(key: key);
+  const AccountPage({super.key});
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
-
+    // final theme = Theme.of(context);
+    // final textTheme = theme.textTheme;
+    final l10n = context.l10n;
     return MaterialApp(
       home: Scaffold(
         body: _buildListView(context),
         bottomNavigationBar: BottomNavigationBar(
           type: BottomNavigationBarType.fixed,
-          currentIndex: 0,
           items: [
             BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Trang chủ',
+              icon: const Icon(Icons.home),
+              label: l10n.homeLabel,
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.restore),
-              label: 'Hoạt động',
+              icon: const Icon(Icons.restore),
+              label: l10n.activateLabel,
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.notifications),
-              label: 'Thông báo',
+              icon: const Icon(Icons.notifications),
+              label: l10n.notificationLabel,
             ),
             BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle),
-              label: 'Tài khoản',
+              icon: const Icon(Icons.account_circle),
+              label: l10n.accountLabel,
             )
           ],
           onTap: (int index) {},
@@ -48,120 +47,164 @@ class AccountPage extends StatelessWidget {
         Container(
           alignment: Alignment.center,
           padding: const EdgeInsets.fromLTRB(10, 60, 10, 10),
-          child: CircleAvatar(
-            radius: 80,
-            backgroundColor: Color.fromARGB(255, 242, 226, 80),
-            child: Text(
-              'N',
-              style: TextStyle(fontSize: 90, color: Colors.white),
+          // child: const CircleAvatar(
+          //   radius: 80,
+          //   backgroundColor: Color.fromARGB(255, 242, 226, 80),
+          //   child: Text(
+          //     'N',
+          //     style: TextStyle(fontSize: 90, color: Colors.white),
+          //   ),
+          // ),
+          // child: const CircleAvatar(
+          //   radius: 40,
+          //   backgroundColor: Colors.white,
+          child: const CircleAvatar(
+            radius: 38,
+            backgroundImage: AssetImage('assets/images/user-image-default.png'),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 12,
+                child: Icon(
+                  Icons.camera_alt,
+                  size: 15,
+                  color: Color(0xFF404040),
+                ),
+              ),
             ),
+          ),
+          // ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(10),
+          child: Text(
+            l10n.nameAccountText,
+            style: const TextStyle(fontSize: 30),
           ),
         ),
         Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              l10n.nameAccountText,
-              style: TextStyle(fontSize: 30),
-            )),
-        Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(10),
-            child: Text(
-              l10n.phoneText,
-              style: TextStyle(fontSize: 18),
-            )),
+          alignment: Alignment.center,
+          padding: const EdgeInsets.all(10),
+          child: Text(
+            l10n.phoneText,
+            style: const TextStyle(fontSize: 18),
+          ),
+        ),
         Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16),
           child: Text(
             l10n.accountLabel,
           ),
         ),
         ListTile(
           title: Text(l10n.serviceAccountText),
-          leading: Icon(Icons.home_repair_service),
-          trailing:
-              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+          leading: const Icon(Icons.home_repair_service),
+          trailing: IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () {},
+          ),
           onTap: () {},
         ),
         ListTile(
           title: Text(l10n.editProfileText),
-          leading: Icon(Icons.portrait),
-          trailing:
-              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+          leading: const Icon(Icons.portrait),
+          trailing: IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () {},
+          ),
           onTap: () {},
         ),
         ListTile(
           title: Text(l10n.paymentText),
-          leading: Icon(Icons.payment),
-          trailing:
-              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+          leading: const Icon(Icons.payment),
+          trailing: IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () {},
+          ),
           onTap: () {},
         ),
         ListTile(
           title: Text(l10n.organizationText),
-          leading: Icon(Icons.business),
-          trailing:
-              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+          leading: const Icon(Icons.business),
+          trailing: IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () {},
+          ),
           onTap: () {},
         ),
         ListTile(
           title: Text(l10n.changePassWordText),
-          leading: Icon(Icons.key),
-          trailing:
-              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+          leading: const Icon(Icons.key),
+          trailing: IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () {},
+          ),
           onTap: () {},
         ),
         ListTile(
           title: Text(l10n.changeLanguageText),
-          leading: Icon(IconData(0xe366, fontFamily: 'MaterialIcons')),
-          trailing:
-              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+          leading: const Icon(IconData(0xe366, fontFamily: 'MaterialIcons')),
+          trailing: IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () {},
+          ),
           onTap: () {},
         ),
-        Divider(
+        const Divider(
           height: 1,
           thickness: 1,
         ),
         Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16),
           child: Text(
             l10n.guideAndSuportLabel,
           ),
         ),
         ListTile(
           title: Text(l10n.faqsText),
-          leading: Icon(Icons.quiz),
-          trailing:
-              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+          leading: const Icon(Icons.quiz),
+          trailing: IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () {},
+          ),
           onTap: () {},
         ),
         ListTile(
           title: Text(l10n.termsOfServiceText),
-          leading: Icon(Icons.fact_check),
-          trailing:
-              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+          leading: const Icon(Icons.fact_check),
+          trailing: IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () {},
+          ),
           onTap: () {},
         ),
         ListTile(
           title: Text(l10n.aboutUsText),
-          leading: Icon(Icons.people),
-          trailing:
-              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+          leading: const Icon(Icons.people),
+          trailing: IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () {},
+          ),
           onTap: () {},
         ),
         ListTile(
           title: Text(l10n.suportsText),
-          leading: Icon(Icons.help),
-          trailing:
-              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+          leading: const Icon(Icons.help),
+          trailing: IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () {},
+          ),
           onTap: () {},
         ),
         ListTile(
           title: Text(l10n.logoutText),
-          leading: Icon(Icons.logout),
-          trailing:
-              IconButton(icon: Icon(Icons.arrow_forward_ios), onPressed: () {}),
+          leading: const Icon(Icons.logout),
+          trailing: IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () {},
+          ),
           onTap: () {},
         ),
       ],
--- lib/account/view/signup6_page.dart
+++ lib/account/view/signup6_page.dart
@@ -1,10 +1,12 @@
+// ignore_for_file: unnecessary_new
+
 import 'package:flutter/material.dart';
 import 'package:revup/l10n/l10n.dart';
 
 void main() => runApp(const Signup6Page());
 
 class Signup6Page extends StatelessWidget {
-  const Signup6Page({Key? key}) : super(key: key);
+  const Signup6Page({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,15 +22,16 @@ class Signup6Page extends StatelessWidget {
     return ListView(
       children: <Widget>[
         Container(
-            padding: const EdgeInsets.fromLTRB(16, 50, 16, 0),
-            child: Text(
-              l10n.completeRegistrationText,
-              style: TextStyle(fontSize: 30),
-            )),
+          padding: const EdgeInsets.fromLTRB(16, 50, 16, 0),
+          child: Text(
+            l10n.completeRegistrationText,
+            style: const TextStyle(fontSize: 30),
+          ),
+        ),
         Container(
           alignment: Alignment.center,
           padding: const EdgeInsets.fromLTRB(16, 60, 10, 16),
-          child: CircleAvatar(
+          child: const CircleAvatar(
             radius: 80,
             backgroundColor: Color.fromARGB(255, 242, 226, 80),
             child: Text(
@@ -43,45 +46,51 @@ class Signup6Page extends StatelessWidget {
             child: Column(
               children: [
                 TextFormField(
-                  style: TextStyle(fontSize: 22),
+                  style: const TextStyle(fontSize: 22),
                   decoration: InputDecoration(
-                      labelText: l10n.fullNameText,
-                      labelStyle: TextStyle(fontSize: 18)),
+                    labelText: l10n.fullNameText,
+                    labelStyle: const TextStyle(fontSize: 18),
+                  ),
                 ),
                 TextFormField(
-                  style: TextStyle(fontSize: 22),
+                  style: const TextStyle(fontSize: 22),
                   decoration: InputDecoration(
-                      labelText: l10n.emailText,
-                      labelStyle: TextStyle(fontSize: 18)),
+                    labelText: l10n.emailText,
+                    labelStyle: const TextStyle(fontSize: 18),
+                  ),
                 ),
                 TextFormField(
-                  style: TextStyle(fontSize: 22),
+                  style: const TextStyle(fontSize: 22),
                   decoration: InputDecoration(
                     hintText: 'Enter date',
                     labelText: l10n.dateText,
-                    labelStyle: TextStyle(fontSize: 18),
+                    labelStyle: const TextStyle(fontSize: 18),
                   ),
                   keyboardType: TextInputType.datetime,
                 ),
                 TextFormField(
-                  style: TextStyle(fontSize: 22),
+                  style: const TextStyle(fontSize: 22),
                   decoration: InputDecoration(
-                      labelText: l10n.addressText,
-                      labelStyle: TextStyle(fontSize: 18)),
+                    labelText: l10n.addressText,
+                    labelStyle: const TextStyle(fontSize: 18),
+                  ),
                 ),
-                Container(
-                  child: Padding(
-                      padding: EdgeInsets.fromLTRB(16, 100, 16, 10),
-                      child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            fixedSize: const Size(850, 50),
-                            primary: Color.fromARGB(255, 87, 68, 16),
-                            shape: new RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(30.0),
-                            ),
-                          ),
-                          child: Text('DONE'))),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 100, 16, 10),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(850, 50),
+                      primary: const Color.fromARGB(255, 87, 68, 16),
+                      shape: new RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: Text(
+                      l10n.doneText,
+                      style: const TextStyle(fontSize: 18),
+                    ),
+                  ),
                 ),
               ],
             ),
