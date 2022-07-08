5a6da022b4e71d8d1ebc33bff06c959490d9dee6
huynq <huynqhe140638@fpt.edu.vn>
2022-07-08 16:24:32 +0700

    refactor: change phone number params

--- lib/login/view/login_password_page.dart
+++ lib/login/view/login_password_page.dart
@@ -7,12 +7,13 @@ import '../bloc/login_password_bloc.dart';
 import 'login_password_view.dart';
 
 class LoginPasswordPage extends StatelessWidget {
-  const LoginPasswordPage({super.key});
+  const LoginPasswordPage(this.phoneNumber,{super.key});
+  final String phoneNumber;
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => LoginPasswordBloc(context.l10n.invalidPasswordLabel),
-      child: const LoginPasswordView('+84 163999999'),
+      child: LoginPasswordView(phoneNumber),
     );
   }
 }
