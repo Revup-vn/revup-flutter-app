e14f48bbaf7d843c6c1ad14233e35e46f8dde8fb
cantgim <cantgim@gmail.com>
2022-07-27 02:12:08 +0700

    refactor: apply review changed

--- test/service/choose-product/bloc/choose_product_bloc_test.dart
+++ test/service/choose-product/bloc/choose_product_bloc_test.dart
@@ -23,29 +23,13 @@ void main() {
       productImageUrl:
           'https://images.unsplash.com/photo-1657826181686-a1d28a11e3ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80',
     ),
-    const ProductData(
-      id: '3',
-      serviceId: '1',
-      name: 'Chips',
-      price: 1000,
-      productImageUrl:
-          'https://images.unsplash.com/photo-1657826181686-a1d28a11e3ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80',
-    ),
-    const ProductData(
-      id: '4',
-      serviceId: '1',
-      name: 'Chips',
-      price: 1000,
-      productImageUrl:
-          'https://images.unsplash.com/photo-1657826181686-a1d28a11e3ae?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80',
-    ),
   ];
 
   setUp(() {
     chooseProductBloc = ChooseProductBloc();
   });
   blocTest<ChooseProductBloc, ChooseProductState>(
-    'emits Loading when Started is added.',
+    'emits [loading, success] when Started is added.',
     build: () => chooseProductBloc,
     act: (bloc) => bloc.add(const ChooseProductEvent.started()),
     expect: () => [
@@ -54,7 +38,7 @@ void main() {
     ],
   );
   blocTest<ChooseProductBloc, ChooseProductState>(
-    'emits Loading when Submitted is added.',
+    'emits [loading] when Submitted is added.',
     build: () => chooseProductBloc,
     act: (bloc) => bloc.add(const ChooseProductEvent.submitted('')),
     expect: () => [
