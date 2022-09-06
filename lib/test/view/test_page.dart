2e44d9fadbc6e77c9e46adad937c78953d6b3167
huynq <huynqhe140638@fpt.edu.vn>
2022-09-06 16:53:20 +0700

    refactor: format code

--- lib/test/view/test_page.dart
+++ lib/test/view/test_page.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 
-import '../../h26_review-repairman/view/review_repairman_page.u.dart';
-
 class TestPage extends StatelessWidget {
   const TestPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ReviewRepairmanPage(
-        '2JTdDcZ9t8MOiVu0i67qWDjYz793', '209c41e4-1d54-4b4b-a6ee-d5aaac6ec412');
+    return Container();
   }
 }
